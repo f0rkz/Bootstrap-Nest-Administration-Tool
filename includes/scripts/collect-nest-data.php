2cb920df4a33c0832f998f0cc92f2b40d275b9f0
f0rkz <f0rkz@f0rkznet.net>
2014-11-17 18:04:55 -0500

    Prune out old section of script

--- includes/scripts/collect-nest-data.php
+++ includes/scripts/collect-nest-data.php
@@ -74,34 +74,3 @@ while ( $row = mysqli_fetch_array($get_users))
 		}
 	}
 }
-	
-
-/*
-$nest = new Nest();
-
-$infos = $nest->getDeviceInfo();
-$weather_array = json_decode(file_get_contents($weather_json));
-
-if ($weather_array)
-{
-	// Current weather conditions
-	$outside_temperature_kelvin = $weather_array->main->temp;
-	$outside_humidity = $weather_array->main->humidity;
-	$timestamp = time();
-	$temperature = $infos->current_state->temperature;
-	$humidity = $infos->current_state->humidity;
-	$ac = $infos->current_state->ac;
-	$heat = $infos->current_state->heat;
-	$scale = $infos->scale;
-	$target = $infos->target->temperature;
-	$time_to_target = $infos->target->time_to_target;
-	$outside_temperature = convertKelvin($outside_temperature_kelvin, $scale);
-
-	$fields = "timestamp=\"$timestamp\", heating=\"$heat\", cooling=\"$ac\", target=\"$target\", current=\"$temperature\", humidity=\"$humidity\", outside_temp=\"$outside_temperature\", outside_humidity=\"$outside_humidity\"";
-	$server_sql = "INSERT INTO data SET $fields";
-	if (!mysqli_query($db_connect, $server_sql))
-	{
-		die('Error: ' . mysqli_error($db_connect));
-	}
-}
-*/
