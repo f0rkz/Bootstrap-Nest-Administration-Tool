48c6a4df21f804bd17d5121545b82bb52d4b808b
f0rkz <f0rkz@f0rkznet.net>
2014-12-03 15:40:44 -0500

    Move outside temperature statistic collection over to the Nest api. Commented the collection script a little. Pave the way for nest control panel (barf)

--- includes/scripts/collect-nest-data.php
+++ includes/scripts/collect-nest-data.php
@@ -29,16 +29,16 @@ while ( $row = $users_statement->fetch())
 	$nest_password_decrypt = trim(decrypt(utf8_decode($nest_password), ENCRYPTION_KEY));
 
 	// Open weathermap api call
+	// Get a latitude and longitude from openweathermap for mapping to timezone offset
 	$weather_json = "http://api.openweathermap.org/data/2.5/weather?q=" . urlencode($user_location);
 	$weather_array = json_decode(file_get_contents($weather_json));
 	$user_lat = $weather_array->coord->lat;
 	$user_long = $weather_array->coord->lon;
-	$outside_temperature_kelvin = $weather_array->main->temp;
-	$outside_humidity = $weather_array->main->humidity;
 
 	$timestamp = time();
 
 	// Google maps api URL
+	// This is required to get the timezone offset from the current user's location
 	$google_json = "https://maps.googleapis.com/maps/api/timezone/json?location=" . $user_lat . "," . $user_long . "&timestamp=" . $timestamp;
 	$google_time = json_decode(file_get_contents($google_json));
 	$dst_offset = $google_time->dstOffset;
@@ -54,20 +54,28 @@ while ( $row = $users_statement->fetch())
 
 		foreach($nest_devices as $device)
 		{
+			// Gather information from the Nest class object for storage
 	  		$infos = $nest->getDeviceInfo($device);
+	  		$energy = $nest->getEnergyLatest($device);
+	  		$weather_nest = $nest->getWeather($user_location);
 
+	  		// Gather the device information for storage
 			$device_serial_number = $infos->serial_number;
 			$nest_location = $infos->where;
 			$device_name = $infos->name;
-			
-			$outside_temperature_kelvin = $weather_array->main->temp;
-			$outside_humidity = $weather_array->main->humidity;
+
+			// Outside weather pulled from the nest class
+			$outside_humidity = $weather_nest->outside_humidity;
+			$outside_temperature = $weather_nest->outside_temperature;
+
+			// Inside weather pulled from the nest class
 			$temperature = $infos->current_state->temperature;
 			$humidity = $infos->current_state->humidity;
+
+			// Current running statistics for the graph
 			$ac = $infos->current_state->ac == "" ? 0 : $infos->current_state->ac;
 			$heat = $infos->current_state->heat == "" ? 0 : $infos->current_state->heat;
-			$scale = $infos->scale;$time_to_target = $infos->target->time_to_target;
-			$outside_temperature = convertKelvin($outside_temperature_kelvin, $scale);
+			$scale = $infos->scale;$time_to_target = $infos->target->time_to_target;			
 
 			// if heat/cool is off API returns the away temp in an array.
 			// consisting of [low temp, high temp] - commented out line is to keep both, but for now let's just take the low temp
